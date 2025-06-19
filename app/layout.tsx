291a34a3082949c7a96416feb3d9dab0daee4a6d
ClaudioG3 <claudiogonzalez@injury-hub.com>
2025-06-19 14:56:57 -0400

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,54 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import React from "react";
+
+import "./globals.css";
+import Theme from "@/components/navigation/navbar/theme";
+import { ThemeProvider } from "next-themes";
+import Navbar from "@/components/navigation/navbar";
+
+const inter = localFont({
+  src: "./fonts/InterVF.ttf",
+  variable: "--font-inter",
+  weight: "100 200 300 400 500 700 800 900",
+});
+
+const spaceGrotesk = localFont({
+  src: "./fonts/SpaceGroteskVF.ttf",
+  variable: "--font-space-grotesk",
+  weight: "300 400 500 700",
+});
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description:
+    "A community-driven platform for askind and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web PHASE_DEVELOPMENT_SERVER, mobile app development, algorithms, data structures, and monitorEventLoopDelay.",
+
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}{" "}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
