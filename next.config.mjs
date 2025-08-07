2dae6048c2e8c6c17c92d01462416c11998d1897
claudio-gonzalez <gnzlz.cld@gmail.com>
2025-08-06 22:04:58 -0400

    Implement Log Out and Show User Info on Home

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,22 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ["pino", "pino-pretty"],
+  // serverExternalPackages: ["pino", "pino-pretty"],
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "static.vecteezy.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
         port: "",
       },
     ],
